d65100ddaa9dfda7466fa1c513df0c272e04abd8
fjarlaegur <127120402+fjarlaegur@users.noreply.github.com>
2025-03-10 23:51:03 +0700

    added parametrizing to test_product_page; marked one of the links as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,17 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_products_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+parameter_list = [f"?promo=offer{i}" for i in range(10)]
+link_list = [link + parameter for parameter in parameter_list]
+
+#  marking bugged promo as xfail
+link_list[7] = pytest.param(link_list[7], marks=pytest.mark.xfail)
+
+
+@pytest.mark.parametrize("link", link_list)
+def test_guest_can_add_products_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
